d7eba1c9f05540538100c5a42fa5b238ebc4db93
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-24 16:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-//complete this code
-class Person {}
+// Person class definition
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+}
+
+// Student subclass that extends Person
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method to log that the student is studying
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher subclass that extends Person
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method to log that the teacher is teaching
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
